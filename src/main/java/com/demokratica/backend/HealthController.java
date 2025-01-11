ffd5ec5569e9b33c53108112faf0ddb4d440e0fb
arojasag <arojasag@unal.edu.co>
2025-01-10 23:20:02 -0500

    Documentación añadida a todo, reconfigurado para que se conecte la BD de Neón

--- src/main/java/com/demokratica/backend/HealthController.java
+++ src/main/java/com/demokratica/backend/HealthController.java
@@ -5,6 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+    Este controlador lo definí solo para asegurarme de que la aplicación funciona correctamente
+    y es accesible. Al enviar una petición GET al endpoint /health/ me devuelve un OK
+    TODO: eliminar esta clase
+ */
 @RestController
 public class HealthController {
     
