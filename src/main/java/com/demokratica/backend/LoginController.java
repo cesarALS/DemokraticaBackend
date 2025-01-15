8746ca229bf0a6b7093f93806424c0d36bd2e7bb
cesarArthuroLemos <clemos@unal.edu.co>
2025-01-15 12:22:55 -0500

    resolviendo el problema de CORS temporalmente. Eventualmente, el dominio podría ser el mismo que el del front, lo que eliminaría por completo el problema CORS

--- src/main/java/com/demokratica/backend/LoginController.java
+++ src/main/java/com/demokratica/backend/LoginController.java
@@ -8,11 +8,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "https://demokratica.vercel.app"}, allowCredentials = "true")
 public class LoginController {
 
     @Autowired
