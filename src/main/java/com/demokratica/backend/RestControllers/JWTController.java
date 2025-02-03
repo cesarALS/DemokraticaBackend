893791a04cb6065a9187dff9de89367c070a7e71
arojasag <arojasag@unal.edu.co>
2025-02-03 00:04:59 -0500

    Intento de solucionar problemas de CORS en endpoint token-info

--- src/main/java/com/demokratica/backend/RestControllers/JWTController.java
+++ src/main/java/com/demokratica/backend/RestControllers/JWTController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /*
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * podría hacer)
  */
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "https://demokratica.vercel.app"}, allowCredentials = "true")
 public class JWTController {
 
     @GetMapping("/token-info")
