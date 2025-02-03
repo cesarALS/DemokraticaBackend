5d224bd52e926ab1a87d34a9bfb81194d5ffb392
arojasag <arojasag@unal.edu.co>
2025-02-03 02:26:32 -0500

    Avances iniciales en implementar las nuevas entidades y relaciones necesarias para crear sesiones

--- src/main/java/com/demokratica/backend/Model/User.java
+++ src/main/java/com/demokratica/backend/Model/User.java
@@ -1,6 +1,10 @@
 package com.demokratica.backend.Model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -16,5 +20,12 @@ public class User {
     private String email;
     private String username;
     private String password;
+    //Este atributo lo pide Spring Security para guardar usuarios
     private Boolean enabled;
+
+    @Enumerated(EnumType.STRING) //Especificamos que se guarde como String y no como número
+    private Plan plan;
+    enum Plan {GRATUITO, PLUS, PREMIUM, PROFESIONAL};
+    private LocalDateTime expirationDate; //Puede ser null en el caso de lplan gratuito
+    
 }
