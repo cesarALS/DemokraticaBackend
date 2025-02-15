358e27083e30de332655575eed09573f811ac6c0
cesarArthuroLemos <clemos@unal.edu.co>
2025-02-15 00:04:36 -0500

    Revert "Merge pull request #24 from cesarALS/NewAPIs"
    This reverts commit 3a8803601b95c215fee4b468025dcd539cc6ab24, reversing
    changes made to 4635b4e2a5872faca258e614af1fe0c27abbf0b4.

--- src/main/java/com/demokratica/backend/Repositories/PollsRepository.java
+++ src/main/java/com/demokratica/backend/Repositories/PollsRepository.java
@@ -1,20 +1,11 @@
 package com.demokratica.backend.Repositories;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.demokratica.backend.Model.Invitation;
 import com.demokratica.backend.Model.Poll;
 
 @Repository
 public interface PollsRepository extends JpaRepository<Poll, Long> {
     
-    @Query("SELECT i.role FROM Poll p " +
-           "JOIN Invitation i ON i.session = p.session " +
-            "WHERE p.id = :poll_id AND i.invitedUser.email = :userEmail")
-    Optional<Invitation.Role> findPollOwner(@Param("poll_id") Long poll_id, @Param("userEmail") String userEmail);
 }
