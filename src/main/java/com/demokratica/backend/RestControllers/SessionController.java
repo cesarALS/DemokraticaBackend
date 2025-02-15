1757a0d6fc8654a1bbe25d55c422b95eefd9434e
arojasag <arojasag@unal.edu.co>
2025-02-15 04:28:52 -0500

    Solucionado SessionController

--- src/main/java/com/demokratica/backend/RestControllers/SessionController.java
+++ src/main/java/com/demokratica/backend/RestControllers/SessionController.java
@@ -9,7 +9,7 @@ import com.demokratica.backend.Services.SessionService;
 import com.demokratica.backend.Services.SessionService.GetSessionsDTO;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -34,7 +36,7 @@ public class SessionController {
     public ResponseEntity<?> returnAllUserSessions() {
         try {
             String userEmail = SecurityConfig.getUsernameFromAuthentication();
-            List<GetSessionsDTO> getSessionsResponse = sessionService.getSessionsOfUser(userEmail);
+            ArrayList<GetSessionsDTO> getSessionsResponse = sessionService.getSessionsOfUser(userEmail);
             return new ResponseEntity<>(getSessionsResponse, HttpStatus.OK);
         } catch (UnsupportedAuthenticationException e) {
             return e.getResponse();
@@ -43,11 +45,26 @@ public class SessionController {
 
     @PostMapping("/api/sessions")
     public ResponseEntity<?> createNewSession(@RequestBody NewSessionDTO newSessionDTO) {
-        sessionService.createSession(newSessionDTO);
-
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        try {
+            String userEmail = SecurityConfig.getUsernameFromAuthentication();
+            sessionService.createSession(userEmail, newSessionDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (UnsupportedAuthenticationException e) {
+            return e.getResponse();
+        }
     }
 
+    @PutMapping("api/sessions/{id}")
+    public ResponseEntity<?> updateSession(@PathVariable Long id, @RequestBody NewSessionDTO dto) {
+        try {
+            String userEmail = SecurityConfig.getUsernameFromAuthentication();
+            sessionService.updateSession(id, userEmail, dto);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (UnsupportedAuthenticationException e) {
+            return e.getResponse();
+        }
+    }
+    
     //Retornar 204 (No Content) si se borró exitosamente, 403 (Forbidden) en caso de que no tenga autorización o el recurso no exista
     //No se envía un 404 (Not Found) porque esa  información podría ser usada por un atacante para averiguar las IDs de los recursos
     //existentes
@@ -65,7 +82,7 @@ public class SessionController {
         
     }
     
-    public record NewSessionDTO (String title, String description, LocalDateTime startTime, LocalDateTime endTime, List<InvitationDTO> invitations, List<TagDTO> tags) {
+    public record NewSessionDTO (String title, String description, LocalDateTime startTime, LocalDateTime endTime, ArrayList<InvitationDTO> invitations, ArrayList<TagDTO> tags) {
     }
 
     public record InvitationDTO (Invitation.Role role, String invitedUserEmail) {
