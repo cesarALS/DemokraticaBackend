d155d26526bc543cdafcc382f88235aabed28cfc
arojasag <arojasag@unal.edu.co>
2025-02-15 08:02:12 -0500

    Correción de la URL de algunos endpoints y detalles menores

--- src/main/java/com/demokratica/backend/RestControllers/ActivitiesController.java
+++ src/main/java/com/demokratica/backend/RestControllers/ActivitiesController.java
@@ -10,7 +10,6 @@ import com.demokratica.backend.Services.PollService.PollDTO;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
