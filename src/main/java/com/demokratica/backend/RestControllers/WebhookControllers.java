4729a72e6badf50b169b4c4bfd426716c16c9011
cesarArthuroLemos <clemos@unal.edu.co>
2025-02-28 19:54:20 -0500

    refactor(pasarela webhook) - cors deshabilitado para esta ruta

--- src/main/java/com/demokratica/backend/RestControllers/WebhookControllers.java
+++ src/main/java/com/demokratica/backend/RestControllers/WebhookControllers.java
@@ -4,12 +4,14 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/webhooks")
+@CrossOrigin(origins = "*") // Toca deshabilitarlo completamente
 public class WebhookControllers {
     
     // API que le permite a mercado pago notificar sobre el éxito o fracaso de la transacción
